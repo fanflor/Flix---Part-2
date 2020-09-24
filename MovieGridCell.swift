90fc754a4bdc6955afca2e32cbfa404895898382
fanflor <51783444+fanflor@users.noreply.github.com>
2020-09-24 13:13:49 -0400

    Add files via upload

---
+++ MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixer
+//
+//  Created by searto  on 9/24/20.
+//  Copyright © 2020 Fanflor theodore. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell
+{
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
